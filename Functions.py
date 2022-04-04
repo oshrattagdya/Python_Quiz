9b0e29544dd92a51a26e0a1eb84c387d0eee4ea6
netzanet tadesse <netzanet123@gmail.com>
2022-04-04 12:33:23 +0300

    new update trivia game- qest and ans , functions

---
+++ Functions.py
@@ -0,0 +1,70 @@
+from questionsDictionary_answersList import questions, answers
+
+name= input("Enter your name:")
+print("Hello",name,",this is capital cities trivia game!!! ")
+print("Have fun :-)")
+print()
+
+#פונקציה שקוראת למשחק (רשימת ניחושים, תשובות נכונות, סופר שאלות ומדפיסה )
+
+def newgame():
+    guesses= []
+    right_gueses = 0
+    counter=1
+    for key in questions:
+        print("--------------------")
+        print(key)
+        for i in answers[counter-1]:
+            print(i)
+        guess = input("Enter (A,B,C OR D): ")
+        while guess not in ("A","B","C","D"):
+            guess= input("Enter (A,B,C OR D): ")
+
+        guesses.append(guess)
+
+        right_gueses += check_answer(questions.get(key),guess)
+        if counter ==10 and check_answer(questions.get(key),guess) ==1:
+            right_gueses+=1
+        if counter % 3 == 0:
+            print("Your Mid score is:", round(right_gueses*(100/9),2))
+        counter +=1
+
+    score(right_gueses,guesses)
+
+# פונקציה שמשווה בין הניחוש לתשובה הנכונה ומחזירה ניקוד
+def check_answer(answer,guess):
+
+    if answer == guess:
+        print("Correct answer")
+        return 1
+    else:
+        print("Wrong answer")
+        return 0
+
+
+#פונקציה לחישוב הנקודות -ניחושים לעומת התשובות הנכונות ומחזירה את התשובות הנכונות+
+# את הניחושים של השחקן+ מחשבת ניקוד
+
+def score(right_guesses,guesses):
+    print("--------------------")
+    print("Results")
+    print("--------------------")
+    print("Answers: ", end="")
+    for i in questions:
+        print(questions.get(i), end=" ")
+    print()
+    print("Guesses: ", end="")
+    for i in guesses:
+        print(i, end=" ")
+    print()
+
+    score1=int((right_guesses*(100/9)))
+    print("Your score is : " + str(score1) +" "+ "POINTS")
+    print()
+    print("G   A  M  E       O   V  E   R ! !")
+
+
+newgame()
+
+
+
--- questionsDictionary_answersList.py
+++ questionsDictionary_answersList.py
@@ -1,53 +1,3 @@
-def newgame():
-    guesses= []
-    right_gueses = 0
-    questions_num=1
-    for key in questions:
-        print("--------------------")
-        print(key)
-        for i in answers[questions_num-1]:
-            print(i)
-        guess = input("Enter (A,B,C OR D): ")
-        guess = guess.upper()
-        guesses.append(guess)
-
-        right_gueses += check_answer(questions.get(key),guess)
-        questions_num +=1
-
-    score(right_gueses,guesses)
-
-
-def check_answer(answer,guess):
-    if answer == guess:
-        print("Correct answer")
-        return 1
-    elif answer[-1]==guess:
-        return 20
-    else:
-        print("Wrong answer")
-        return 0
-
-
-def score(right_guesses,guesses):
-    print("--------------------")
-    print("Results")
-    print("--------------------")
-    print("Answers: ", end="")
-    for i in questions:
-        print(questions.get(i), end=" ")
-    print()
-    print("Guesses: ", end="")
-    for i in guesses:
-        print(i, end=" ")
-    print()
-
-    score1=int((right_guesses*10))
-    print("Your score is : " + str(score1) +" "+ "POINTS")
-
-def newroud():
-    pass
-
-
 questions = {
 "ROUND 1: \n1. In which country would you find the capital city of Sofia? : ": "A",
 "2. What is the capital city of Italy?  :": "B",
@@ -72,8 +22,3 @@ answers=[["A.Bulgaria ","B.Italy ","C.France ","D.Spain  "],
          ["A.London", "B. Ankara", "C.Berlin", "D.Stockholm"],
          ["A.Bern", "B. Warsaw", "C.Riga", "D.Oslo"],
          ["A.Dublin", "B. Brussels", "C.Minsk", "D.Tirana"]]
-
-newgame()
-
-
-
